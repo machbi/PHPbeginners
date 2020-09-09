5f309fd79260df5a76c2946818430e35ec5d71bf
Mark Bijl <m>
2020-09-09 13:01:44 +0200

    Conc symbol moved

--- H02/opdracht4.php
+++ H02/opdracht4.php
@@ -4,7 +4,7 @@ $a = "\"omgaan";
 $b = "met";
 $c = "teleu";
 
-echo 'De cursus' . $a . " met " . $c . 'rstellingen" ' . 'kan vanavond helaas niet doorgaan<br>- Herman Finkers.';
+echo 'De cursus' . $a . " met " . $c . 'rstellingen" ' . 'kan vanavond helaas niet doorgaan.<br>- Herman Finkers';
 
 
 //De cursus “omgaan met teleurstellingen” kan vanavond helaas niet doorgaan.
