73abbd0abc562f8bcbc7c764a2d04212c7f6d3d2
Mark Bijl <m>
2020-09-09 12:52:59 +0200

    Opdrachten hoofdstuk 2 gemaakt

---
+++ H02/opdracht2.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet";
+
+echo 'M' . $b . 's' . $b . ' ' . $a . $b . ' ' . 'mak' . $b . ', '. 'werk' . $b . ' ' . 'niet.'.'<br>';
+echo ' - Albert Plesman (KLM)';
+
+//Mensen die geen fouten maken werken niet
+//- Albert Plesman (KL)
---
+++ H02/opdracht4.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+echo 'De cursus' . $a . " met " . $c . 'rstellingen" ' . 'kan vanavond helaas niet doorgaan<br>- Herman Finkers';
+
+
+//De cursus “omgaan met teleurstellingen” kan vanavond helaas niet doorgaan.
+//- Herman Finkers
