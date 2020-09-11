cd795e204877013d62be768fb43bfa5d6fdf5a81
Mark Bijl <m>
2020-09-11 10:10:32 +0200

    Add aap#.jpgs to folder h03

---
+++ H03/opdracht1.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>H4 Opdracht 1</title>
+</head>
+<body>
+</body>
+</html>
+
+<?php
+for ($i = 0; $i < 10 ; $i++) {
+    echo '<img src="apen/aap1' . $i . '.jpg" alt="aap">' . $i;
+}
+?>
+
+
