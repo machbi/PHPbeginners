e1d69696ce0a4f0a2c1d78ab90d377a5b2c25794
Mark Bijl <m>
2020-09-18 16:42:03 +0200

    Add opdracht7.php

---
+++ H03/opdracht7.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>H03 - opdracht 8</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+
+$kappersAgenda = [
+    "9:15" => "Mevr. Pietersen",
+    "9.30" => "Mevr. Willems",
+    "9.45" => "",
+    "10.00" => "Paul van den Broek",
+    "10.15" => "Karel de Meeuw",
+    "10.30" => ""
+];
+
+print("De volgende momenten zijn nog beschikbaar:<ul>");
+foreach($kappersAgenda as $tijd => $afspraak) {
+if($afspraak == "") {
+print("<li>".$tijd."</li>") ;
+}
+}
+print("</ul>");
